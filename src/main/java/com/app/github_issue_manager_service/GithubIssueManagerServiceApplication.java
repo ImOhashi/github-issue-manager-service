3eccc4b2557ecf3743382bffa4ed37443b3032ee
ImOhashi <leonardoohashi.mkt@gmail.com>
2024-10-24 15:38:49 -0300

    first commit

---
+++ src/main/java/com/app/github_issue_manager_service/GithubIssueManagerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.app.github_issue_manager_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubIssueManagerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubIssueManagerServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/github_issue_manager_service/GithubIssueManagerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.github_issue_manager_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubIssueManagerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
